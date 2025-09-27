2336f3fd9456acde6ea56be92939ae702e1c26b3
Adalumo Festus <105289292+Hydro-Codes@users.noreply.github.com>
2025-09-27 03:33:39 -0700

    Add files via upload

---
+++ nextjs-birthday-wish-main/pages/index.js
@@ -0,0 +1,100 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+import Router from "next/router";
+import useTheme from "../hooks/useTheme";
+import { useState } from "react";
+import { Button } from "../components";
+
+export default function Home() {
+  const { themes, setTheme, currentTheme } = useTheme();
+  const [value, setValue] = useState("");
+
+  const handleInput = (e) => {
+    e.preventDefault();
+    const id = currentTheme.id;
+
+    if (!value || value[0] === " ") {
+      alert("Please enter a name!");
+      return;
+    }
+    if (id == 0) Router.push(value);
+    // If the theme is default blue then push to '/{name}'
+    else Router.push(`/${value}/${id}`); // If the theme is not default then will push to '/{name}?color={id}
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Happy Birthday</title>
+      </Head>
+
+      <main>
+        <div className={styles.main}>
+          <h1 className={styles.title}>
+            Choose a <span className={styles.span}>Birthday Theme</span>
+          </h1>
+        </div>
+        {/* Theme Color  */}
+
+        <div className={styles.themeWrapper}>
+          <form
+            className={styles.theme}
+            id="theme-input"
+            onChange={(e) => setTheme(e.target.id)}
+          >
+            {themes.map((item) => (
+              <input
+                key={item.id}
+                type="radio"
+                className={item.name}
+                id={item.id}
+                name="theme"
+                value={item.color}
+                defaultChecked={currentTheme.id === item.id}
+              />
+            ))}
+          </form>
+        </div>
+
+        <div>
+          <form className={styles.form} onSubmit={handleInput}>
+            <input
+              id="input"
+              name="go"
+              className={styles.input}
+              placeholder="Enter name of the person"
+              value="GENERAL ISMO ALHAJI"
+              defaultValue="GENRAL ISMO ALHAJI"
+              onChange={(e) => setValue(e.target.value)}
+            />
+            <Button className={styles.button} type="submit" text="Go!" />
+          </form>
+          <p className={styles.desc}>
+            Created by{" "}
+            <a
+              className={styles.span}
+              href="wa.me/8105467040"
+              target="_blank"
+              rel="noreferrer"
+            >
+              HYDROGEN
+            </a>
+            .
+          </p>
+          <p className={styles.desc}>
+            Thanks to all the{" "}
+            <a
+              className={styles.span}
+              href=""
+              target="_blank"
+              rel="noreferrer"
+            >
+              contributors
+            </a>
+            !
+          </p>
+        </div>
+      </main>
+    </div>
+  );
+}
